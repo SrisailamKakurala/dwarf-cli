b05a033f640593d1cba9a8d9b50e375b8b4cceff
srisailamkakurala <srisailamkakurala@gmail.com>
2025-03-02 14:52:24 +0530

    rst->md and add .learnings

---
+++ .learning/practice.js
@@ -0,0 +1,5 @@
+const chalk = require("chalk");
+
+console.log(chalk.green("Success!"));
+console.log(chalk.red("Error: Something went wrong!"));
+console.log(chalk.blue.bold("Info: Processing..."));
